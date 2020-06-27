1f3d69e18c32bcaadbc3b01f779fe10cbd3281e6
Caio Cesar <caio.cdcs@gmail.com>
2020-06-26 21:11:22 -0300

    getting android geolocation in backgroud

--- project/app/src/main/java/com/example/covid_19tracker/MainActivity.kt
+++ project/app/src/main/java/com/example/covid_19tracker/MainActivity.kt
@@ -10,18 +10,30 @@ import android.view.View
 
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), PermissionRequestFragment.Callbacks,
+    LocationUpdateFragment.Callbacks {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
         val fab: View = findViewById(R.id.fab)
-        fab.setOnClickListener() {
+        fab.setOnClickListener {
             intent = Intent(this, SignUpActivity::class.java)
             startActivity(intent)
         }
 
+        val currentFragment = supportFragmentManager.findFragmentById(R.id.fragment_container)
+
+        if (currentFragment == null) {
+
+            val fragment = LocationUpdateFragment.newInstance()
+
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.fragment_container, fragment)
+                .commit()
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -43,6 +55,44 @@ class MainActivity : AppCompatActivity() {
     public fun launchSecondActivity(view: View){
         intent = Intent(this,SignUpActivity::class.java)
         startActivity(intent)
+    }
+
+    // Triggered from the permission Fragment that it's the app has permissions to display the
+    // location fragment.
+    override fun displayLocationUI() {
+
+        val fragment = LocationUpdateFragment.newInstance()
+
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
+    }
+
+    // Triggers a splash screen (fragment) to help users decide if they want to approve the missing
+    // fine location permission.
+    override fun requestFineLocationPermission() {
+        val fragment = PermissionRequestFragment.newInstance(PermissionRequestType.FINE_LOCATION)
+
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .addToBackStack(null)
+            .commit()
+    }
+
+    // Triggers a splash screen (fragment) to help users decide if they want to approve the missing
+    // background location permission.
+    override fun requestBackgroundLocationPermission() {
+        val fragment = PermissionRequestFragment.newInstance(
+            PermissionRequestType.BACKGROUND_LOCATION
+        )
+
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .addToBackStack(null)
+            .commit()
     }
 
 }
