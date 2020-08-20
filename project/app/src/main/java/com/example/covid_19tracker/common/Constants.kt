f1cfa005caf6ac3fe9f854460ebb8aa9d247f768
Wilson Martins <martins.wilson17@gmail.com>
2020-08-20 11:47:54 -0300

    Add deployment URL

--- project/app/src/main/java/com/example/covid_19tracker/common/Constants.kt
+++ project/app/src/main/java/com/example/covid_19tracker/common/Constants.kt
@@ -1,7 +1,7 @@
 package com.example.covid_19tracker.common
 
 object Constants {
-    val BASE_URL = "http://10.0.2.2:8080/";
+    val BASE_URL = "https://covid-api-tracker.herokuapp.com/";
     val DISEASE_ID = 1
     val PREFERENCES = "preferences_covid_19tracker"
 }
