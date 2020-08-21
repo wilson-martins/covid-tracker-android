01c4ea8c1fed83c8298e87de1044385391fe1919
Caio Cesar <caio.cdcs@gmail.com>
2020-08-21 14:10:56 -0300

    updating base url

--- project/app/src/main/java/com/example/covid_19tracker/common/Constants.kt
+++ project/app/src/main/java/com/example/covid_19tracker/common/Constants.kt
@@ -1,7 +1,7 @@
 package com.example.covid_19tracker.common
 
 object Constants {
-    val BASE_URL = "https://covid-api-tracker.herokuapp.com/";
+    val BASE_URL = "http://34.121.248.169:80/";
     val DISEASE_ID = 1
     val PREFERENCES = "preferences_covid_19tracker"
 }
